775a2761155b74ed1f4f492601c4b22354c1291e
Chuan Wang <chuan.wang101@gmail.com>
2022-04-25 12:06:06 -0700

    out put html

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const inquirer = require("inquirer");
+const fs = require('fs');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 const generate = require('./util/generateHtml.js');
 var team = [];
 
-const init = async() => {
+const init = async () => {
     try {
         const ans = await inquirer.prompt([
             {
@@ -36,70 +37,81 @@ const init = async() => {
     }
 }
 
-const addTeamMember = async() => {
-    let ans = await inquirer.prompt([
-        {
-            type: "list",
-            name: "choice",
-            message: "What would you like to do next?",
-            choices: ["Add an Engineer", "Add an Intern", "Finish Building Team"],
+const addTeamMember = async () => {
+    try {
+        let ans = await inquirer.prompt([
+            {
+                type: "list",
+                name: "choice",
+                message: "What would you like to do next?",
+                choices: ["Add an Engineer", "Add an Intern", "Finish Building Team"],
+            }
+        ]);
+        switch (ans.choice) {
+            case "Add an Engineer":
+                const ans1 = await inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "name",
+                        message: "What is the engineer's name?",
+                    },
+                    {
+                        type: "input",
+                        name: "id",
+                        message: "What is the engineer's id?",
+                    },
+                    {
+                        type: "input",
+                        name: "email",
+                        message: "What is the engineer's email?",
+                    },
+                    {
+                        type: "input",
+                        name: "github",
+                        message: "What is the engineer's github user name?",
+                    },
+                ]);
+                team.push(new Engineer(ans1.name, ans1.id, ans1.email, ans1.github));
+                addTeamMember();
+                break;
+            case "Add an Intern":
+                const ans2 = await inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "name",
+                        message: "What is the intern's name?",
+                    },
+                    {
+                        type: "input",
+                        name: "id",
+                        message: "What is the intern's id?",
+                    },
+                    {
+                        type: "input",
+                        name: "email",
+                        message: "What is the intern's email?",
+                    },
+                    {
+                        type: "input",
+                        name: "school",
+                        message: "What is the intern's school name?",
+                    },
+                ]);
+                team.push(new Intern(ans2.name, ans2.id, ans2.email, ans2.school));
+                addTeamMember();
+                break;
+            default:
+                const content = generate(team);
+                fs.writeFile('./dist/index.html', content, (err) => {
+                    if (err)
+                        console.log(err);
+                    else {
+                        console.log("HTML generated successfully in dist folder!");
+                    }
+                });
         }
-    ]);
-    switch (ans.choice) {
-        case "Add an Engineer":
-            const ans1 = await inquirer.prompt([
-                {
-                    type: "input",
-                    name: "name",
-                    message: "What is the engineer's name?",
-                },
-                {
-                    type: "input",
-                    name: "id",
-                    message: "What is the engineer's id?",
-                },
-                {
-                    type: "input",
-                    name: "email",
-                    message: "What is the engineer's email?",
-                },
-                {
-                    type: "input",
-                    name: "github",
-                    message: "What is the engineer's github user name?",
-                },
-            ]);
-            team.push(new Engineer(ans1.name, ans1.id, ans1.email, ans1.github));
-            addTeamMember();
-            break;
-        case "Add an Intern":
-            const ans2 = await inquirer.prompt([
-                {
-                    type: "input",
-                    name: "name",
-                    message: "What is the intern's name?",
-                },
-                {
-                    type: "input",
-                    name: "id",
-                    message: "What is the intern's id?",
-                },
-                {
-                    type: "input",
-                    name: "email",
-                    message: "What is the intern's email?",
-                },
-                {
-                    type: "input",
-                    name: "school",
-                    message: "What is the intern's school name?",
-                },
-            ]);
-            team.push(new Intern(ans2.name, ans2.id, ans2.email, ans2.school));
-            addTeamMember();
-            break;
-        default:
-            console.log(team);
+    } catch (err) {
+        console.log(err);
     }
 }
 
